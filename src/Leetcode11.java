7f2a7a2078fa3b4b98953700f5a85e5eb2f0db0e
“Zionified” <zzheng3@andrew.cmu.edu>
2022-08-16 01:37:19 -0400

    implemented container with most water

--- src/Leetcode11.java
+++ src/Leetcode11.java
@@ -1,3 +1,8 @@
+/* Description:
+ * Find two lines that together with the x-axis form a container, such that the 
+ * container contains the most water.
+ * Return the maximum amount of water a container can store.
+ */ 
 public class Leetcode11 {
     public int maxArea(int[] height) {
         int max = Integer.MIN_VALUE;
@@ -13,5 +18,5 @@ public class Leetcode11 {
         }
         return max;
     }
-    
+
 }
