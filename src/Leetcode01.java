23eafa3a211fa00af5ab55304026ca1d57ff3302
“Zionified” <zzheng3@andrew.cmu.edu>
2022-08-16 23:08:12 -0400

    classic TwoSum problem: added ArrayList approach

--- src/Leetcode01.java
+++ src/Leetcode01.java
@@ -5,12 +5,13 @@
  * use the same element twice.
  * You can return the answer in any order. 
  */
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Leetcode01 {
 
-    // HashMap solution:
+    // HashMap solution: 4ms 43mb
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> complement = new HashMap<>();
         int[] result = new int[2];
@@ -24,4 +25,25 @@ public class Leetcode01 {
         }
         return result;
     }
+
+    // ArrayList solution: 360ms 45.8mb
+    public int[] twoSum_ArrayList(int[] nums, int target) {
+        ArrayList<Integer> complement = new ArrayList<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (complement.contains(nums[i])) {
+                result[0] = i;
+
+                /** add() ensures that the index of nums[i] is the index of its 
+                    complement in the original array */
+                result[1] = complement.indexOf(nums[i]);
+            } else {
+
+                // add by default will append elements at the end
+                complement.add(target - nums[i]);
+            }
+        }
+        return result;
+    }
+
 }
